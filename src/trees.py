89d74957264253c8ab6babe82f2cf38efb0b244b
dagsdags <jegsamson.dev@gmail.com>
2024-04-08 03:12:26 +0800

    hierarchical selectors for parsing html

---
+++ src/trees.py
@@ -0,0 +1,15 @@
+TREE = {
+    "article_list": {
+        "root": "div.highwire-article-citation-list div.highwire-list ul",
+        "title": "span.highwire-cite-title a span",
+        "url": "span.highwire-cite-title a",
+        "doi": "div.highwire-cite-metadata span.highwire-cite-metadata-doi",
+        "author_list": {
+            "root": "div.highwire-cite-authors",
+            "first_name": "span.nlm-given-names",
+            "surname": "span.nlm-surname"
+        }
+    },
+}
+
+
