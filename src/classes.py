d40da21946952e5fe78e900c768ed6f03b1ee8b0
dagsdags <jegsamson.dev@gmail.com>
2024-04-10 14:53:45 +0800

    added  attribute to Article class

--- src/classes.py
+++ src/classes.py
@@ -1,9 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
 import pandas as pd
 
 @dataclass
 class Author:
+    """Stores author information."""
     first_name: Optional[str]
     surname: str
 
@@ -19,17 +20,20 @@ class Author:
     def __repr__(self) -> str:
         if self.first_name and self.surname:
             return f"{self.first_name} {self.surname}"
-        return "" 
+        return ""
 
 @dataclass
 class Article:
-    title: Optional[str]
-    authors: list[Author]
-    url: str
+    """Stores the metadata for the article content."""
+    title: str              = ""
+    abstract: str           = ""
+    authors: list[Author]   = field(default_factory=list)
+    doi_url: str            = ""
 
     def __post_init__(self) -> None:
-        if self.title is None: 
+        if self.title is None:
             return
+        # add a newline for every `chars_per_line` characters
         chars_per_line = 6
         title = ""
         tokens = self.title.split(" ")
@@ -38,25 +42,26 @@ class Article:
                 title += f" {t}"
                 if (i != 0) and (i % chars_per_line == 0):
                     title += "\n\t"
-        self.title = title
+            self.title = title
 
     def to_json(self) -> dict[str, str]:
         """Returns a JSON object containing class attributes."""
         return {
             "title": self.title,
             "authors": [str(author) for author in self.authors],
-            "url": self.url,
+            "url": self.doi_url,
         }
 
     def to_series(self) -> pd.Series:
         """Returns a pandas Series containing class attributes."""
         first_author = self.authors[0].surname
-        d = [self.title, f"{first_author} et al.", self.url]
+        d = [self.title, f"{first_author} et al.", self.doi_url]
         index = ["title", "authors", "url"]
         return pd.Series(d, index)
 
 @dataclass
 class ArticleTable:
+    """Formats article list into a DataFrame."""
     data: list[Article]
 
     def to_df(self) -> pd.DataFrame:
@@ -66,5 +71,3 @@ class ArticleTable:
         """
         d = [article.to_series() for article in self.data]
         return pd.DataFrame(d)
-
-
