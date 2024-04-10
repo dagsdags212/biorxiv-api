e4c1d03bc0ee59fb7a52f7e664e33996fb28ea4c
dagsdags <jegsamson.dev@gmail.com>
2024-04-10 14:54:16 +0800

    revised api structure

--- src/api.py
+++ src/api.py
@@ -1,45 +1,76 @@
 import asyncio
 import httpx
 from enums import Collection
+from parser import CollectionParser, ContentParser
+from trees import URL_TREE
 
 class BiorxivApi:
     """A wrapper for sending request to the bioRxiv server."""
-    BASE_URL = "https://www.biorxiv.org"
 
-    def __init__(self, field: Collection) -> None:
+    def __init__(self, field: Collection, n_pages: int=1) -> None:
         self.field = field.lower()
-        self.url = None
-        self.response = []
-        if self.field in Collection:
-            self._compose_url()
-        else:
-            raise ValueError("invalid `field` parameter")
+        self.n_pages = n_pages if n_pages >= 1 else 1
+        self.root_url = None
+        self.collection_response = []
+        self.content_response = []
+        self.article_urls = []
+        self.data = []
+
+        self._compose_url()
+        asyncio.run(self._fetch_collection())
+        self._extract_article_urls()
+        asyncio.run(self._fetch_content())
+        self._extract_article_content()
 
     def _compose_url(self) -> None:
         """Builds a url from the given field."""
-        url = f"{self.BASE_URL}/collection/{self.field}"
-        self.url = url
+        field = self.field.lower()
+        if field in Collection:
+            url = f"{URL_TREE["collection"]}/{field}"
+            self.root_url = url
+        else:
+            raise ValueError("invalid `field` parameter")
 
-    async def _get(self, client: httpx.AsyncClient, params: dict) -> httpx.Response:
+    async def _get(self, client: httpx.AsyncClient, url: str, params: dict={}) -> httpx.Response:
         """Sends an asynchronous request to the server."""
-        resp = await client.get(self.url, params=params)
+        resp = await client.get(url, params=params, timeout=10.0)
         resp.raise_for_status()
         return resp
 
-    async def fetch(self, n_pages=1) -> str:
+    async def _fetch_collection(self) -> None:
         """
-        Builds a query url based on the given field and sends a request
+        Builds a query url based on the given field and send a series of request
         to the bioRxiv server.
         
-        Returns a status code upon receiving a valid Response object.
+        Returns a list of httpx Response objects.
         """
-        print("Fetching data from server...")
+        print("Fetching article list from server...")
         async with httpx.AsyncClient() as client:
             response = await asyncio.gather(
-                *[self._get(client, {"page": i}) for i in range(n_pages)]
+                *[self._get(client, self.root_url, {"page": i}) for i in range(self.n_pages)]
             )
-            self.response = response
-        print("All responses have been collected!")
-            
-        return response
+            self.collection_response = response
+        print("Done!")
 
+    def _extract_article_urls(self) -> None:
+        """Parses a list of response objects to extract the article url."""
+        parser = CollectionParser(self.collection_response)
+        self.article_urls = parser.extract_data()
+
+    async def _fetch_content(self) -> None:
+        """
+        Sends a request to fetch data containing the article content. The url
+        for each article is stored in the `article_urls` attribute. Response
+        objects are stored in `content_response` attribute.
+        """
+        print("Fetching content for each article...")  
+        async with httpx.AsyncClient() as client:
+            response = await asyncio.gather(
+                *[self._get(client, url) for url in self.article_urls]
+            )
+            self.content_response = response
+        print("Done!")
+
+    def _extract_article_content(self) -> None:
+        parser = ContentParser(self.content_response)
+        self.data = parser.extract_data()
--- src/biorx.py
+++ src/biorx.py
@@ -1,49 +1,43 @@
-import asyncio
+from sys import exit
 from tabulate import tabulate
 # custom modules
 from api import BiorxivApi
 from classes import ArticleTable
-from parser import MainParser
-from trees import TREE
 from ArgumentParser import arg_parser
 
 
 def main() -> None:
+    # parse args into a dictionary
     args = arg_parser.parse_args()
     field = args.field.lower()
     n_pages = args.pages if args.pages else 1
-    api = BiorxivApi(field)
-    # collect responses from concurrent requests
-    response = asyncio.run(api.fetch(n_pages)) 
-    if len(response) > 0:
-        resp = api.response
-        parser = MainParser(resp, TREE)
-        data = parser.data
-        df = ArticleTable(data).to_df()
-        # truncate table if -t flag is passed
-        if args.truncate:
-            if ":" in args.truncate:
-                start, end = args.truncate.split(":")
-                start, end = int(start), int(end)
-            else:
-                start = 1
-                end = int(args.truncate)
-            df = df.truncate(start, end)
-        # sort table by column if -s flag is passed
-        if args.sort:
-            # sort by multiple columns
-            if "," in args.sort:
-                columns = [col.lower() for col in args.sort.split(",")]
-                df = df.sort_values(columns)
-            # sort by a single column
-            else:
-                df = df.sort_values(args.sort)
-        # format and dispaly dataframe
-        table = tabulate(df, tablefmt="pipe", headers="keys", numalign="center")
-        print(table)
-    else:
-        print("Reponse failed, exiting...")
+    # create api instance
+    api = BiorxivApi(field, n_pages)
+    # extract list of articles and store into a dataframe
+    article_data = api.data
+    # exit program if api retuns an empty list
+    if len(article_data) == 0:
         exit(1)
+    df = ArticleTable(article_data).to_df()
+    if args.truncate:
+        if ":" in args.truncate:
+            start, end = args.truncate.split(":")
+            start, end = int(start), int(end)
+        else:
+            start = 1
+            end = int(args.truncate)
+        df = df.truncate(start, end)
+    # sort table by column if -s flag is passed
+    if args.sort:
+        # sort by multiple columns
+        if "," in args.sort:
+            columns = [col.lower() for col in args.sort.split(",")]
+            df = df.sort_values(columns)
+        # sort by a single column
+        else:
+            df = df.sort_values(args.sort)
+    table = tabulate(df, tablefmt="pipe", headers="keys", numalign="center")
+    print(table)
 
 if __name__ == "__main__":
     main()
